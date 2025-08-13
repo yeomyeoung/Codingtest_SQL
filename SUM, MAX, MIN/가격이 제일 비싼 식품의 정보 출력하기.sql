37694877481870cb4c918d02d47cf6c839c481a3
박여명 <qkrduaud123@gmail.com>
2025-08-13 17:27:23 +0900

    add 가격이 제일 비싼 식품의 정보 출력하기 AggregationFunction_1

---
+++ SUM, MAX, MIN/가격이 제일 비싼 식품의 정보 출력하기.sql
@@ -0,0 +1,7 @@
+-- 문제
+--FOOD_PRODUCT 테이블에서 가격이 제일 비싼 식품의 식품 ID, 식품 이름, 식품 코드, 식품분류, 식품 가격을 조회하는 SQL문을 작성해주세요.
+
+-- 답
+SELECT PRODUCT_ID, PRODUCT_NAME, PRODUCT_CD, CATEGORY, PRICE
+FROM FOOD_PRODUCT AS F
+WHERE PRICE = (SELECT MAX(PRICE) FROM FOOD_PRODUCT);
